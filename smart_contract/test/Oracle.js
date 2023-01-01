dbb44375a9050e740774cb26bb3b0171720f2020
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-01 10:28:48 -0300

    test: deploy and test both contracts

--- smart_contract/test/Oracle.js
+++ smart_contract/test/Oracle.js
@@ -5,12 +5,38 @@ const tokens = (n) => {
     return ethers.utils.parseUnits(n, toString(), 'ether')
 }
 
-describe('Oracle', () => {
+describe('Oracle Escrow', () => {
+    // variables
+    let buyer, seller
+    let fashionProducts
+    let oracleShipment
 
     it('Saves the addresses.', async () => {
+
+        // Get Signers
+        // const signers = await ethers.getSigners()
+        
+        // Setup accounts
+        [buyer, seller] = await ethers.getSigners()
+
+        // Deploy FashionProducts
         const FashionProducts = await ethers.getContractFactory('FashionProducts')
         fashionProducts = await FashionProducts.deploy()
 
-        console.log(fashionProducts.address)
+        // Mint
+        let transaction = await fashionProducts.connect(seller).mint("https://ipfs.io/ipfs/QmTudSYeM7mz3PkYEWXWqPjomRPHogcMFSq7XAvsvsgAPS")
+        await transaction.wait()
+
+        // Deploy OracleShipment
+        const OracleShipment = await ethers.getContractFactory('OracleShipment')
+        oracleShipment = await OracleShipment.deploy(fashionProducts.address, seller.address)
+
+        const result = await oracleShipment.nftAddress()
+        expect(result).that.be.equal(fashionProducts.address)
+
+        const resultSeller = await oracleShipment.seller()
+        expect(resultSeller).that.be.equal(seller.address)
+
+        console.log(result)
     })
 })
