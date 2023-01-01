37bf0321fb41243cae2c9b16ea11829f2434ed93
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2022-12-31 21:09:03 -0300

    struct: add contract files for the system

---
+++ smart_contract/contracts/FashionProducts.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.17;
+
+/* Contract for the information and transactions of fashion products */
+contract FashionProducts {
+
+}
---
+++ smart_contract/contracts/OracleShipment.sol
@@ -0,0 +1,7 @@
+//SPDX-License-Identifier: Unlicensed
+pragma solidity ^0.8.17;
+
+/* Contract to get shipment tracking status */
+contract OracleShipment {
+
+}
