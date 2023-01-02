edd597d99139adf1ed004a05050b8b60386aceb6
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-02 08:15:16 -0300

    struct: add OracleContract.sol file
    This file will be responsible for fetching shipment data from the web.

---
+++ smart_contract/contracts/OracleContract.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+/* Contract for the oracle to get shipment (delivery) status 
+    - fetches an URL to receive a .json file
+    - parses the file to get the "delivered" key value
+*/
+contract OracleContract {
+
+}
