048dba013ab5b527c01f3a00cf2e704458660eb4
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-02 13:48:02 -0300

    build: basic structure for testing

--- smart_contract/contracts/TrackingOracle.sol
+++ smart_contract/contracts/TrackingOracle.sol
@@ -4,4 +4,7 @@ pragma solidity ^0.8.17;
 import "../node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "../node_modules/@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
 
-contract TrackingOracle {}
+contract TrackingOracle {
+    // State variables
+    string private http;
+}
--- smart_contract/test/TrackingOracle.js
+++ smart_contract/test/TrackingOracle.js
@@ -5,9 +5,17 @@ describe('Tracking Oracle', () => {
   // variables
 
   /* Before each test */
+  beforeEach(async () => {
+    // Setup
+    
+    
+  })
 
   /* Tests */
   it('Returns ETH price in USD.', async () => {
+    let signers = await ethers.getSigners()
+    let oracle = signers[2]
 
+    console.log(oracle.address)
   })
 })
