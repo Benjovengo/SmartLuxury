ebc1f89c72ef8e7877a9676274997f7a029fc2df
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-02 10:52:18 -0300

    build: remove test for TrackingOracle

--- smart_contract/test/TrackingOracle.js
+++ smart_contract/test/TrackingOracle.js
@@ -4,4 +4,10 @@ const { ethers } = require('hardhat');
 describe('Tracking Oracle', () => {
   // variables
 
+  /* Before each test */
+
+  /* Tests */
+  it('Returns ETH price in USD.', async () => {
+
+  })
 })
