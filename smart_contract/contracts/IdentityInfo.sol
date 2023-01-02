157deb90bc38f423e778955b0fe976792c89491c
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-02 08:09:40 -0300

    chore: add comments about account info

--- smart_contract/contracts/IdentityInfo.sol
+++ smart_contract/contracts/IdentityInfo.sol
@@ -1,7 +1,10 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-/* Contract for the contact information based on the blockchain address */
+/* Contract for the contact information based on the blockchain address 
+    -> Information given on the Sign Up
+    -> Displayed on the Login
+*/
 contract IdentityInfo {
 
 }
