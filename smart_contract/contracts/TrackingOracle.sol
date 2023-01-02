f0a97e06312645013cbbdd14be3df794f2596ffb
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-02 10:51:50 -0300

    build: remove contents from TrackingOracle
    Contract and Testing removed.

--- smart_contract/contracts/TrackingOracle.sol
+++ smart_contract/contracts/TrackingOracle.sol
@@ -1,41 +1,7 @@
-//SPDX-License-Identifier: UNLICENSED
+// SPDX-License-Identifier: MIT
 pragma solidity ^0.8.17;
 
-// Import Chainlink
 import "../node_modules/@chainlink/contracts/src/v0.8/ChainlinkClient.sol";
 import "../node_modules/@chainlink/contracts/src/v0.8/ConfirmedOwner.sol";
-import "../node_modules/@chainlink/contracts/src/v0.8/interfaces/AggregatorV3Interface.sol";
 
-/* Contract for the oracle to get shipment (delivery) status 
-    - fetches an URL to receive a .json file
-    - parses the file to get the "delivered" key value
-*/
-contract TrackingOracle {
-    AggregatorV3Interface internal priceFeed;
-
-    /**
-     * Network: Goerli
-     * Aggregator: ETH/USD
-     * Address: 0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-     */
-    constructor() {
-        priceFeed = AggregatorV3Interface(
-            0xD4a33860578De61DBAbDc8BFdb98FD742fA7028e
-        );
-    }
-
-    /**
-     * Returns the latest price
-     */
-    function getLatestPrice() public view returns (int256) {
-        (
-            ,
-            /*uint80 roundID*/
-            int256 price, /*uint startedAt*/ /*uint timeStamp*/ /*uint80 answeredInRound*/
-            ,
-            ,
-
-        ) = priceFeed.latestRoundData();
-        return price;
-    }
-}
+contract TrackingOracle {}
