da54e3d9cb477eda625c0c8e32e9f16bc5565dbf
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-03 12:34:46 -0300

    build: config hardhat to use the Goerli network

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,6 +1,13 @@
-require("@nomicfoundation/hardhat-toolbox");
+require("@nomicfoundation/hardhat-toolbox")
+require('dotenv').config()
 
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
+  networks: {
+    goerli: {
+      url: 'https://eth-goerli.g.alchemy.com/v2/5Z28yMrHkUYPHuwdYFNZmQjFo3H5BzWc',
+      accounts: [process.env.PRIVATE_KEY],
+    },
+  },
 };
