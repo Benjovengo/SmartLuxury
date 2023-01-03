fbe3e6e44cc69acb295c5a177cebed267aa3dc0a
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-03 13:36:34 -0300

    build: add copy functionality to deploy
    Copy funcionality to make the ABI available to the client-side web application.

--- smart_contract/scripts/deploy.js
+++ smart_contract/scripts/deploy.js
@@ -4,6 +4,7 @@
 // We require the Hardhat Runtime Environment for running the script in a 
 // standalone fashion through `node <script>`.
 const hre = require("hardhat");
+const fs = require("fs"); // to copy the files to be used by the web interface
 
 const main = async () => {
 
@@ -19,9 +20,22 @@ const main = async () => {
   console.log("ContactInfo address: ", )
 }
 
+// Function to copy the ABI files
+function copyABIFiles(_trackingABI, _destinationPath) {
+  fs.copyFile(_trackingABI, _destinationPath, (err) => {
+  if (err) throw err;
+  console.log('ABI successfully copied to client-side application');
+  })
+}
+
 const runMain = async () => {
   try {
     await main()
+    // copy files to client-side
+    const sourceTrackingABI = "./artifacts/contracts/TrackingOracle.sol/TrackingOracle.json"
+    const destinationFolder = "../work/TrackingOracle.json"
+    copyABIFiles(sourceTrackingABI, destinationFolder)
+    // terminate without errors
     process.exit(0)
   } catch (error) {
     console.error(error)
