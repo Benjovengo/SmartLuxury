a1c94d54e2749329c02869dd5198b2aab4a4b4ea
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-05 10:19:03 -0300

    build: add routers

---
+++ client/src/routes/Routers.js
@@ -0,0 +1,22 @@
+import React from 'react'
+
+// import pages
+import Contact from '../components/Contact'
+import EditProfile from '../components/EditProfile'
+import Home from '../components/Home'
+import ListItem from '../components/ListItem'
+import Market from '../components/Market'
+import Navigation from '../components/Navigation'
+import ProductNFTDetails from '../components/ProductNFTDetails'
+import Search from '../components/Search'
+import SellerProfile from '../components/SellerProfile'
+import Wallet from '../components/Wallet'
+
+
+const Routers = () => {
+  return (
+    <div>Routers</div>
+  )
+}
+
+export default Routers
