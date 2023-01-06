830d2d0a25bed2f9c8c3e5df7a55b5648919d112
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-06 10:29:11 -0300

    build: add remixicon module

--- client/src/index.js
+++ client/src/index.js
@@ -5,6 +5,8 @@ import { BrowserRouter as Router } from 'react-router-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import "bootstrap/dist/css/bootstrap.css"
+import "remixicon/fonts/remixicon.css"
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
