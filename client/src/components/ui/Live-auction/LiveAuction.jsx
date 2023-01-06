7d80ac28b5c21f3553a9432f49156065c2253a81
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-06 20:35:56 -0300

    feat: add live auction

---
+++ client/src/components/ui/Live-auction/LiveAuction.jsx
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Container, Row, Col } from 'reactstrap'
+import { Link } from 'react-router-dom'
+
+import './live-auction.css'
+
+import NftCard from '../Nft-card/NftCard'
+//import { NFT__DATA } from '../../../assets/data/data.js' // get data from IPFS
+import { NFT__DATA } from '../../../assets_test/data/data.js' // test data
+
+const LiveAuction = () => {
+  return (
+    <section>
+      <Container>
+        <Row>
+          <Col lg='12' className='mb-4'>
+            <div className="live_auction__top d-flex align-items center justify-content-between">
+              <h3>Live Auction</h3>
+              <span>
+                <Link to='/market'>
+                  Explore more
+                </Link>
+              </span>
+            </div>
+          </Col>
+
+          <Col lg='3'>
+            {
+              NFT__DATA.slice(0,4).map((item)=>(
+                <NftCard key={item.id} itm={item} />
+              ))
+            }
+          </Col>
+
+        </Row>
+      </Container>
+    </section>
+  )
+}
+
+export default LiveAuction
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import { Container } from 'reactstrap'
+
 import HeroSection from '../components/ui/HeroSection'
+import LiveAuction from '../components/ui/Live-auction/LiveAuction'
 
 const Home = () => {
   return (
     <>
       <HeroSection />
+      <LiveAuction />
     </>
   )
 }
