6734ed25095ce27ce351472e276b9ca4cfc50846
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-06 15:45:22 -0300

    design: add tweaks to hero section

--- client/src/components/ui/HeroSection.jsx
+++ client/src/components/ui/HeroSection.jsx
@@ -12,17 +12,16 @@ const HeroSection = () => {
           <Col  lg='6' md='6'>
             <div className="hero__content">
               <h2>Renew your collection<br/>
-                <span>buy and sell your extraordinary</span><br/>
-                luxury items
+                <span>buy and sell</span> luxury items
               </h2>
               
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Nemo ad fugit at quae saepe perferendis quaerat corrupti temporibus similique nihil?</p>
               
               <div className="hero__btns d-flex align-items-center gap-4">
-                <button className='d-flex align-items-center gap-2'>
+                <button className=' explore__btn  d-flex align-items-center gap-2'>
                   <Link to='/market'>Explore</Link>
                 </button>
-                <button className='d-flex align-items-center gap-2'>
+                <button className=' explore__btn  d-flex align-items-center gap-2'>
                   <Link to='/create'>Sell</Link>
                 </button>
               </div>
