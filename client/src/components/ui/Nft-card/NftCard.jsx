2e50e8b2e3e05ad188cc568791dcb04b89ab81f8
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-07 09:24:29 -0300

    style: add indentation

--- client/src/components/ui/Nft-card/NftCard.jsx
+++ client/src/components/ui/Nft-card/NftCard.jsx
@@ -14,7 +14,7 @@ const NftCard = (props) => {
 
       <div className="nft__content">
         <h5 className='nft__title'>
-        <Link to={`/market/${id}`}>{title}</Link>
+          <Link to={`/market/${id}`}>{title}</Link>
         </h5>
         <div className="creator__info-wrapper d-flex gap-3">
           <div className="creator__img">
