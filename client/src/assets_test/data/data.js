3018a45695af0d3dfe7c6654d6b9ecedf9a62e8d
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-11 18:02:57 -0300

    build: remove import images

--- client/src/assets_test/data/data.js
+++ client/src/assets_test/data/data.js
@@ -1,22 +1,5 @@
 /* Get info for products */
 
-import img01 from "../images/Dior-Vintage-Sunglasses_01.webp";
-import img02 from "../images/Flap-Jackie_01.webp";
-import img03 from "../images/img-03.jpg";
-import img04 from "../images/img-04.jpg";
-import img05 from "../images/img-05.jpg";
-import img06 from "../images/img-06.jpg";
-import img07 from "../images/img-07.jpg";
-import img08 from "../images/img-08.jpg";
-import img09 from "../images/img-09.jpg";
-
-import ava01 from "../images/ava-01.png";
-import ava02 from "../images/ava-02.png";
-import ava03 from "../images/ava-03.png";
-import ava04 from "../images/ava-04.png";
-import ava05 from "../images/ava-05.png";
-import ava06 from "../images/ava-06.png";
-
 const folder = [
   "./metadata/Dior-Hobo-Black_IA002000811.json",
   "./metadata/Dior-Vintage-Sunglasses_IA002000251.json",
