ffca05b9a03cf429bcf73b644090534ee5fa556b
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-12 09:15:14 -0300

    feat: add Seller section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -3,12 +3,15 @@ import { Container } from 'reactstrap'
 
 import HeroSection from '../components/ui/HeroSection'
 import LiveAuction from '../components/ui/Live-auction/LiveAuction'
+import SellerSection from '../components/ui/Seller-section/SellerSection'
+
 
 const Home = () => {
   return (
     <>
       <HeroSection />
       <LiveAuction />
+      <SellerSection />
     </>
   )
 }
