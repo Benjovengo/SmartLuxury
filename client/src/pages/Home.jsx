48cef31e31393868237ce4648710c4e0c5f8a27f
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-12 09:52:37 -0300

    feat: add step section

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -5,7 +5,7 @@ import HeroSection from '../components/ui/HeroSection'
 import LiveAuction from '../components/ui/Live-auction/LiveAuction'
 import SellerSection from '../components/ui/Seller-section/SellerSection'
 import TrendingSection from '../components/ui/Trending-section/Trending'
-
+import StepSection from '../components/ui/Step-section/StepSection'
 
 const Home = () => {
   return (
@@ -14,6 +14,7 @@ const Home = () => {
       <LiveAuction />
       <SellerSection />
       <TrendingSection />
+      <StepSection />
     </>
   )
 }
