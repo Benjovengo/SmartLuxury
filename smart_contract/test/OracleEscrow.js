7e942ba6c4e92e15e2fbc378f623d3e872ba0513
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-13 16:21:20 -0300

    test: test escrow functionality

--- smart_contract/test/OracleEscrow.js
+++ smart_contract/test/OracleEscrow.js
@@ -67,6 +67,123 @@ describe('Oracle Escrow', () => {
             expect(await fashionToken.ownerOf(1)).to.be.equal(oracleEscrow.address)
         })
 
+        it('Updates as listed.', async () => {
+            // List product
+            transaction = await oracleEscrow.connect(seller).list(1, tokens(10))
+            await transaction.wait()
+            const result = await oracleEscrow.isListed(1)
+            expect(result).that.be.equal(true)
+        })
+
+        it('Returns purchase price.', async () => {
+            // List product
+            transaction = await oracleEscrow.connect(seller).list(1, tokens(10))
+            await transaction.wait()
+            const result = await oracleEscrow.purchasePrice(1)
+            expect(result).that.be.equal(tokens(10))
+        })
+    })
+
+    describe('Selling info', () => {
+        it('Updates escrow contract balance.', async () => {
+            // List product
+            transaction = await oracleEscrow.connect(seller).list(1, tokens(10))
+            await transaction.wait()
+            transaction = await oracleEscrow.connect(buyer).depositEarnest(1, { value: tokens(10) })
+            await transaction.wait()
+            const result = await oracleEscrow.getBalance()
+            expect(result).to.be.equal(tokens(10))
+            // console.log(result)
+        })
+  
+        it('Returns buyer.', async () => {
+            // List product
+            transaction = await oracleEscrow.connect(seller).list(1, tokens(10))
+            await transaction.wait()
+            transaction = await oracleEscrow.connect(buyer).depositEarnest(1, { value: tokens(10) })
+            await transaction.wait()
+            const result = await oracleEscrow.buyer(1)
+            expect(result).that.be.equal(buyer.address)
+        })
+    })
+
+    describe('Oracle Inspection', () => {
+        beforeEach(async () => {
+            // List product
+            let transaction = await oracleEscrow.connect(seller).list(1, tokens(10))
+            await transaction.wait()
+
+            transaction = await oracleEscrow.connect(oracle).updateDeliveryStatus(1, true)
+            await transaction.wait()
+        })
+  
+        it('Updates inspection status', async () => {
+            const result = await oracleEscrow.wasDelivered(1)
+            expect(result).to.be.equal(true)
+        })
+    })
+
+    describe('Approval', () => {
+        beforeEach(async () => {
+            let transaction = await oracleEscrow.connect(seller).list(1, tokens(10))
+            await transaction.wait()
+            
+            transaction = await oracleEscrow.connect(buyer).approveSale(1)
+            await transaction.wait()
+  
+            transaction = await oracleEscrow.connect(seller).approveSale(1)
+            await transaction.wait()
+  
+            transaction = await oracleEscrow.connect(oracle).approveSale(1)
+            await transaction.wait()
+        })
+  
+        it('Updates approval status', async () => {
+            expect(await oracleEscrow.approval(1, buyer.address)).to.be.equal(true)
+            expect(await oracleEscrow.approval(1, seller.address)).to.be.equal(true)
+            expect(await oracleEscrow.approval(1, oracle.address)).to.be.equal(true)
+        })
+    })
+
+    describe('Sale', () => {
+        beforeEach(async () => {
+            let transaction = await oracleEscrow.connect(seller).list(1, tokens(10))
+            await transaction.wait()
+            
+            transaction = await oracleEscrow.connect(buyer).depositEarnest(1, { value: tokens(10) })
+            await transaction.wait()
+  
+            transaction = await oracleEscrow.connect(oracle).updateDeliveryStatus(1, true)
+            await transaction.wait()
+  
+            transaction = await oracleEscrow.connect(buyer).approveSale(1)
+            await transaction.wait()
+  
+            transaction = await oracleEscrow.connect(seller).approveSale(1)
+            await transaction.wait()
+  
+            transaction = await oracleEscrow.connect(oracle).approveSale(1)
+            await transaction.wait()
+  
+            await oracle.sendTransaction({ to: oracleEscrow.address, value: tokens(5) }) // here a lender can send ether to the contract
+  
+            transaction = await oracleEscrow.connect(seller).finalizeSale(1)
+            await transaction.wait()
+        })
+  
+        it('Updates ownership', async () => {
+            expect(await fashionToken.ownerOf(1)).to.be.equal(buyer.address)
+        })
+  
+        it('Get list of owners', async () => {
+            let result = await fashionToken.getOwners(1)
+            //console.log(result)
+            expect(result[1]).to.be.equal(buyer.address)
+        })
+  
+        it('Updates oracle contract balance', async () => {
+            expect(await oracleEscrow.getBalance()).to.be.equal(0)
+        })
     })
     
 })
