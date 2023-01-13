395c0b459838518192106e5c601bbefad8a1d5bf
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-13 17:24:46 -0300

    refactor: change contract name

---
+++ smart_contract/contracts/VerifiedContacts.sol
@@ -0,0 +1,10 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+/* Verified contract for the contact information based on the blockchain address 
+    -> Information given on by companies
+    -> After a process of verification/validation
+*/
+contract VerifiedContacts {
+
+}
