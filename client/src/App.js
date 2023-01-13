bd8a683a9ec190b52e7d83f1d47d6dbf9b63e801
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-13 19:35:33 -0300

    build: import ABIs and config

--- client/src/App.js
+++ client/src/App.js
@@ -1,8 +1,30 @@
-import logo from './logo.svg';
+import { useEffect, useState } from 'react';
+import { ethers } from 'ethers';
+
+//import logo from './logo.svg';
 import './App.css';
 import Layout from './components/Layout/Layout'
 
+// Import ABIs
+import fashionToken from './abis/FashionToken.json'
+import sellingEscrow from './abis/SellingEscrow.json'
+//import trackingOracle from './abis/TrackingOracle.json'
+// config
+import config from './config.json';
+
+
 function App() {
+
+
+  const loadBlockchainData = async () => {
+    const provider = new ethers.providers.Web3Provider(window.ethereum)
+    console.log(provider)
+  }
+
+  useEffect(() => {
+    loadBlockchainData()
+  }, [])
+
   return (
     <Layout/>
   );
