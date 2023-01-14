7a0e8309777b9eba4707e0c53b6950e3d39bce67
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-14 19:29:47 -0300

    struct: add placeholder for a searchbar

--- client/src/components/Layout/Layout.jsx
+++ client/src/components/Layout/Layout.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import Routers from '../../routes/Routers'
 import Header from '../Header/Header'
 import Footer from '../Footer/Footer'
+import Search from '../Search/Search'
 
 const Layout = ({ account, setAccount }) => {
 
   return (
     <>
       <Header account={account} setAccount={setAccount}/>
+      {/* <Search/> */}
       <div>
         <Routers/>
       </div>
---
+++ client/src/components/Search/Search.js
@@ -0,0 +1,14 @@
+const Search = () => {
+  return (
+      <header>
+          <h2 className="header__title">Search. Explore. Buy!</h2>
+          <input
+              type="text"
+              className="header__search"
+              placeholder="Enter any product you want"
+          />
+      </header>
+  );
+}
+
+export default Search;
