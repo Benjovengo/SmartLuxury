e05768917c903ad2a8855fe59d363a99fe07f43d
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-15 20:26:00 -0300

    style: change default product card

--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -10,10 +10,10 @@ import "../styles/create-item.css";
 
 const item = {
   id: "01",
-  title: "Guard",
+  title: "Product Title",
   desc: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Veniam adipisci cupiditate officia, nostrum et deleniti vero corrupti facilis minima laborum nesciunt nulla error natus saepe illum quasi ratione suscipit tempore dolores. Recusandae, similique modi voluptates dolore repellat eum earum sint.",
   imgUrl: img,
-  creator: "Trista Francis",
+  creator: "Creator's Address",
   creatorImg: avatar,
   currentBid: 7.89,
 };
