4928661d521d2d766dd74c5ff08d46766b526e12
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-16 20:33:30 -0300

    feat: add contacts contract

--- smart_contract/contracts/VerifiedContacts.sol
+++ smart_contract/contracts/VerifiedContacts.sol
@@ -1,18 +1,6 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-/* Regular Contact
-    -> anyone can register a new contact using an address
-    -> the states: mapping using the address as a key
-        - first name
-        - last name
-        - email
-        - physical address -> street, city, state, country, PO Box
-        - products -> IDs of the products owned by that contract (remove after a sale is complete)
-*/
-
-
-
 /* Verified contract for the contact information based on the blockchain address 
     -> Information given on by companies
     -> After a process of verification/validation
