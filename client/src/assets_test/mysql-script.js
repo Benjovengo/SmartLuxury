ae6f7edd0ceb69c2ccc2d5e6180992e721c143bd
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-16 19:50:51 -0300

    bug: mysql package crashes the site

--- client/src/assets_test/mysql-script.js
+++ client/src/assets_test/mysql-script.js
@@ -1,28 +1,8 @@
-require('dotenv').config({ path: "../../.env" })
-let mysql      = require('mysql');
+//require('mysql')
+
+import { MYSQL_USERNAME } from '../work/database'
+
+console.log(MYSQL_USERNAME)
 
 
-function insert_data() {
-  let connection = mysql.createConnection({
-    port     : 3306,
-    host     : '127.0.0.1',
-    user     : process.env.MYSQL_USERNAME,
-    password : process.env.PASSWORD,
-    database : 'products'
-  });
-  connection.connect();
-
-var sql = "INSERT INTO product_description (title, product_id) \
-    VALUES ('Test', 9)";
-connection.query(sql, function (err, result) {
-  if (err) throw err;
-    console.log("1 record inserted");
-  });
-  
-  console.log('Item included in DB.');
-  
-  connection.end();
-}
-
-/* connection.query('SELECT * from product_description', function(err, rows, fields) {
-  if (err) throw err; */
+export const insert__data = MYSQL_USERNAME
--- client/src/pages/Create.jsx
+++ client/src/pages/Create.jsx
@@ -8,7 +8,7 @@ import avatar from "../assets/images/ava-01.png";
 
 import "../styles/create-item.css";
 
-//import { insert_data } from "../assets_test/mysql-script";
+import "../assets_test/mysql-script";
 
 const item = {
   id: "01",
