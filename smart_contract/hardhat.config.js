6c054ef9eeda3cb7d7b6df2b243b2e3ae67fa72d
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-17 12:47:32 -0300

    build: test token

--- smart_contract/hardhat.config.js
+++ smart_contract/hardhat.config.js
@@ -1,16 +1,6 @@
-require("@nomicfoundation/hardhat-toolbox")
-require('dotenv').config()
+require("@nomicfoundation/hardhat-toolbox");
 
-/* @type import('hardhat/config').HardhatUserConfig */
+/** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   solidity: "0.8.17",
-  networks: {
-    hardhat:{
-      blockGasLimit: 8000000000000000
-    },
-    goerli: {
-      url: 'https://eth-goerli.g.alchemy.com/v2/5Z28yMrHkUYPHuwdYFNZmQjFo3H5BzWc',
-      accounts: [process.env.PRIVATE_KEY],
-    },
-  },
 };
