bd6ed389acbc16c1e163bd5d64dd854f184d316a
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-18 16:14:58 -0300

    build: remove unused import function

--- client/src/pages/Account.jsx
+++ client/src/pages/Account.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from "react";
+import React, { useRef } from "react";
 
 import CommonSection from "../components/ui/Common-section/CommonSection";
 import { Container, Row, Col } from "reactstrap";
