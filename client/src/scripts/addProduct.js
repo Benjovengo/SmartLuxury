bd142304ae749f59697b298fe2a5156bb4c4bb57
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-19 19:48:10 -0300

    func: create JSON metadata for product

--- client/src/scripts/addProduct.js
+++ client/src/scripts/addProduct.js
@@ -1,12 +1,60 @@
 import { ethers } from 'ethers';
 
-
 /* Contract */
 import SellingEscrow from '../abis/SellingEscrow.json'
 import config from '../config.json'; // config
 
-export const registerProduct = async (_tokenURI, _serialNumber) => {
+/* Create JSON tokenURI */
 
+export const createJSON = (_name, _brand, _desc, _img, _id, _sku, _category, _condition, _material, _accessory, _weight, _madeIn, _year) => {
+  // Create metadata JSON file
+  var data = {}
+
+  data = {
+    name: _name,
+    brand: _brand,
+    description: _desc,
+    image: _img,
+    id: _id,
+    SKU: _sku,
+    attributes: [
+      {
+        trait_type: "Type of Product",
+        value: _category
+      },
+      {
+        trait_type: "Condition",
+        value: _condition
+      },
+      {
+        trait_type: "Material",
+        value: _material
+      },
+      {
+        trait_type: "Accessory(ies)",
+        value: _accessory
+      },
+      {
+        trait_type: "Weight",
+        value: _weight
+      },
+      {
+        trait_type: "Made In",
+        value: _madeIn
+      },
+      {
+        trait_type: "Year",
+        value: _year
+      }
+    ]
+  }
+
+  console.log(data)
+  return data
+}
+
+/* Register new product */
+export const registerProduct = async (_tokenURI, _serialNumber) => {
   // Setup provider and network
   let provider = new ethers.providers.Web3Provider(window.ethereum)
   const network = await provider.getNetwork()
