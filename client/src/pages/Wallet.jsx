88de8bcaac57bf77ce8afd8bafc0a665986e09f7
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-20 12:52:33 -0300

    feat: add abstract to crypto links

--- client/src/pages/Wallet.jsx
+++ client/src/pages/Wallet.jsx
@@ -13,28 +13,28 @@ import { PRODUCTS__OWNED__FILE, accountData } from '../scripts/accountNFT'
 const wallet__data = [
   {
     title: "Bitcoin",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!",
+    desc: "Experience the future of money with Bitcoin: decentralized, secure, and global digital currency.",
     icon: "ri-bit-coin-line",
     urlLink: "https://bitcoin.org/",
   },
 
   {
     title: "Coinbase",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!",
+    desc: "Coinbase is the easiest place to buy and sell cryptocurrency. Sign up and get started today.",
     icon: "ri-coin-line",
     urlLink: "https://www.coinbase.com/",
   },
 
   {
     title: "Metamask",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!",
+    desc: "Experience seamless blockchain transactions with Metamask, your secure and user-friendly digital wallet.",
     icon: "ri-money-cny-circle-line",
     urlLink: "https://metamask.io/",
   },
 
   {
     title: "Authereum",
-    desc: "Lorem ipsum dolor, sit amet consectetur adipisicing elit. Accusantium accusamus repellat rerum consequatur explicabo? Reiciendis!",
+    desc: "Experience seamless, secure and decentralized Ethereum transactions with Authereum Wallet.",
     icon: "ri-bit-coin-line",
     urlLink: "https://authereum.com/",
   },
@@ -82,7 +82,7 @@ const Wallet = () => {
             
             </Row>
             
-            <Row className="mt-5">
+            <Row className="crypto__links mt-5">
               {wallet__data.map((item, index) => (
               <Col lg="3" md="4" sm="6" key={index} className="mb-4">
                 <a className="wallet__links" href={item.urlLink} target="_blank">
