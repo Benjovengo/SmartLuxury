e8f7d0c6b5251ea753e50a6fe6064f16fdc0ece1
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-23 08:52:11 -0300

    fix: get first owner of a product

--- client/src/assets/data/data.js
+++ client/src/assets/data/data.js
@@ -69,7 +69,7 @@ async function getData() {
         title: json.name,
         description: json.description,
         imgUrl: json.image[0],
-        creator: await fashionToken.ownerOf(productID),
+        creator: await await fashionToken.getFirstOwner(productID),
         firstname: customer[1],
         lastname: customer[2],
         creatorImg: "../images/ava-01.png",
