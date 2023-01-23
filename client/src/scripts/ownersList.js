abe8674a31f697d2498de99884734c3c89fbb218
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-23 07:26:22 -0300

    func: get the list of owners for an item

---
+++ client/src/scripts/ownersList.js
@@ -0,0 +1,32 @@
+import { ethers } from 'ethers';
+
+
+/* Contracts */
+import FashionToken from '../abis/FashionToken.json'
+import config from '../config.json'; // config
+
+/** Setup connection to the smart contracts */
+let provider = new ethers.providers.Web3Provider(window.ethereum, "any")
+const network = await provider.getNetwork()
+const accounts = await window.ethereum.request({ method: 'eth_requestAccounts' })
+// get signer
+const signer = provider.getSigner();
+
+// Javascript "version" of the smart contracts
+const fashionToken = new ethers.Contract(config[network.chainId].fashionToken.address, FashionToken, provider)
+
+/** Get the list of owners for a particular item */
+export const getOwnersList = async (_nftID) => {
+
+  const numberOfOwners = await fashionToken.numberOfOwners(_nftID)
+  let previousOwner
+  let owners = []
+
+  for (let i = numberOfOwners; i > 0; i--){
+    previousOwner = await fashionToken.getOwner(_nftID, i)
+    owners.push(previousOwner)
+  }
+
+  return owners
+
+}
