0bc2d5164bbcf8fc134de0ae90ceae566a5422f9
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-24 20:33:13 -0300

    feat: add page for product not found

--- client/src/pages/NftDetails.jsx
+++ client/src/pages/NftDetails.jsx
@@ -66,74 +66,82 @@ const NftDetails = () => {
 
   return (
     <>
-      <CommonSection title={singleNft.title} />
+      {( singleNft === undefined ) ?
+        <>
+          <CommonSection title='Product not found!' />
+        </>
+      :
+      <>
+        <CommonSection title={singleNft.title} />
 
-      <section>
-        <Container>
-          <Row>
-            <Col lg="6" md="6" sm="6">
-              <img
-                src={singleNft.imgUrl}
-                alt=""
-                className="w-100 single__nft-img"
-              />
-            </Col>
+        <section>
+          <Container>
+            <Row>
+              <Col lg="6" md="6" sm="6">
+                <img
+                  src={singleNft.imgUrl}
+                  alt=""
+                  className="w-100 single__nft-img"
+                />
+              </Col>
 
-            <Col lg="6" md="6" sm="6">
-              <div className="single__nft__content">
-                <h2>{singleNft.title}</h2>
+              <Col lg="6" md="6" sm="6">
+                <div className="single__nft__content">
+                  <h2>{singleNft.title}</h2>
 
-                <div className=" d-flex align-items-center justify-content-between mt-4 mb-4">
-                  <div className=" d-flex align-items-center gap-4 single__nft-seen">
-                    <span>
-                      <i className="ri-eye-line"></i> 234
-                    </span>
-                    <span>
-                      <i className="ri-heart-line"></i> 123
-                    </span>
+                  <div className=" d-flex align-items-center justify-content-between mt-4 mb-4">
+                    <div className=" d-flex align-items-center gap-4 single__nft-seen">
+                      <span>
+                        <i className="ri-eye-line"></i> 234
+                      </span>
+                      <span>
+                        <i className="ri-heart-line"></i> 123
+                      </span>
+                    </div>
+
+                    <div className=" d-flex align-items-center gap-2 single__nft-more">
+                      <span>
+                        <i className="ri-send-plane-line"></i>
+                      </span>
+                      <span>
+                        <i className="ri-more-2-line"></i>
+                      </span>
+                    </div>
                   </div>
 
-                  <div className=" d-flex align-items-center gap-2 single__nft-more">
-                    <span>
-                      <i className="ri-send-plane-line"></i>
-                    </span>
-                    <span>
-                      <i className="ri-more-2-line"></i>
-                    </span>
+                  <div className="nft__creator d-flex gap-3 align-items-center">
+                    <div className="creator__img">
+                      <img src={singleNft.creatorImg} alt="" className="w-100" />
+                    </div>
+
+                    <div className="creator__detail">
+                      <p>Seller - Current Owner</p>
+                      <h6>{singleNft.creator}</h6>
+                      <p>Created By</p>
+                      <h6>{singleNft.creator}</h6>
+                    </div>
                   </div>
+
+                  <p className="my-4">{singleNft.description}</p>
+
+                  <p>Price: {Number(singleNft.currentBid).toFixed(2)} ETH</p>
+
+                  <button className="singleNft-btn d-flex align-items-center gap-2" onClick={() => reviewPurchase()}>
+                    <i className="ri-shopping-bag-line"></i>
+                    <span className='purchase__label'>Purchase Item</span>
+                  </button>
+
+                  {showPurchaseReview && <Review productName={productName} price={price} productId={productId} setShowPurchaseReview={setShowPurchaseReview} />}
+
                 </div>
+              </Col>
+            </Row>
+          </Container>
+        </section>
 
-                <div className="nft__creator d-flex gap-3 align-items-center">
-                  <div className="creator__img">
-                    <img src={singleNft.creatorImg} alt="" className="w-100" />
-                  </div>
-
-                  <div className="creator__detail">
-                    <p>Seller - Current Owner</p>
-                    <h6>{singleNft.creator}</h6>
-                    <p>Created By</p>
-                    <h6>{singleNft.creator}</h6>
-                  </div>
-                </div>
-
-                <p className="my-4">{singleNft.description}</p>
-
-                <p>Price: {Number(singleNft.currentBid).toFixed(2)} ETH</p>
-
-                <button className="singleNft-btn d-flex align-items-center gap-2" onClick={() => reviewPurchase()}>
-                  <i className="ri-shopping-bag-line"></i>
-                  <span className='purchase__label'>Purchase Item</span>
-                </button>
-
-                {showPurchaseReview && <Review productName={productName} price={price} productId={productId} setShowPurchaseReview={setShowPurchaseReview} />}
-
-              </div>
-            </Col>
-          </Row>
-        </Container>
-      </section>
-
-      <LiveAuction />
+        <LiveAuction />
+      </>
+      }
     </>
   );
 };
