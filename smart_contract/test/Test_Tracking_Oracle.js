133527dcb5d613d5886caf221b330219650c6335
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-25 08:36:49 -0300

    test: assert the address is set

--- smart_contract/test/Test_Tracking_Oracle.js
+++ smart_contract/test/Test_Tracking_Oracle.js
@@ -5,7 +5,7 @@ const tokens = (n) => {
   return ethers.utils.parseUnits(n.toString(), 'ether')
 }
 
-describe('Contracts', () => {
+describe('Tracking Oracle', () => {
   // variables
   let deployer, account01
   let fashionToken
@@ -15,7 +15,7 @@ describe('Contracts', () => {
 
   beforeEach(async () => {
     // Setup accounts - to get signers use `const signers = await ethers.getSigners()`
-    [deployer, buyer, seller, oracle] = await ethers.getSigners()
+    [deployer, buyer, seller, account01] = await ethers.getSigners()
 
     // Deploy FashionToken
     const FashionToken = await ethers.getContractFactory('FashionToken')
@@ -31,7 +31,7 @@ describe('Contracts', () => {
 
     // Deploy SellingEscrow
     const SellingEscrow = await ethers.getContractFactory('SellingEscrow')
-    sellingEscrow = await SellingEscrow.deploy(fashionToken.address, contacts.address, oracle.address)
+    sellingEscrow = await SellingEscrow.deploy(fashionToken.address, contacts.address, trackingOracle.address)
 
     // change owner
     fashionToken.changeOwner(sellingEscrow.address)
@@ -45,6 +45,15 @@ describe('Contracts', () => {
     await transaction.wait()
   })
 
+  describe('Deployment and First Steps', () => {
+    it('Has an address.', async () => {
+      const result = await trackingOracle.address
+      expect(result).to.not.equal(null)
+      expect(result).to.not.equal(undefined)
+      expect(result).to.not.equal('')
+      expect(result).to.not.equal('0x')
+    })
 
+  })
 
 })
