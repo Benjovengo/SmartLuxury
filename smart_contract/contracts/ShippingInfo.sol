209f357b343826a6f90c944b10a98b4eb57b70be
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-25 15:29:47 -0300

    contract: add header for the shipment contract

---
+++ smart_contract/contracts/ShippingInfo.sol
@@ -0,0 +1,12 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.17;
+
+/* Information about the shipment 
+    -> Information given on by companies
+    -> After a process of verification/validation
+    -> only the contacts on the Verified Contacts can call this methods
+*/
+
+contract ShippingInfo {
+
+}
