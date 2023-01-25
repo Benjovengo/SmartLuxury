dcb9529fe0703ad5af3f3e1536daff4d59e1bef5
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-01-25 17:51:10 -0300

    contract: add update status

--- smart_contract/contracts/ShippingInfo.sol
+++ smart_contract/contracts/ShippingInfo.sol
@@ -1,11 +1,14 @@
 //SPDX-License-Identifier: UNLICENSED
 pragma solidity ^0.8.17;
 
-/* Information about the shipment 
+/** Information about the shipment 
     -> Information given on by companies
     -> After a process of verification/validation
     -> only the contacts on the Verified Contacts can call this methods
 */
+
+import "./SellingEscrow.sol";
+import "./VerifiedContacts.sol";
 
 contract ShippingInfo {
     // States
@@ -29,19 +32,48 @@ contract ShippingInfo {
 
     /** Constructor Method 
       - sets the owner of the shipment info contract
-  */
+    */
     constructor() {
         owner = msg.sender;
     }
 
     /** Update delivered status
-      - only owner OR the selling contract can call this method
+      - only owner OR the verified carrier OR the selling contract can call this method
       - at any buying operation, check if there is an entry at delivered
         or presential with the SELLER'S ADDRESS AND THAT PARTICULAR NFT NUMBER 
         and set both mappings to false
         (it is important to prevent the approval of a subsequent sale to a
         previous buyer before the delivery of the product)
-     */
+    */
+    function updateDeliveryStatus(
+        address _buyer,
+        uint256 _nftID,
+        bool _newStatus
+    ) public {
+        delivered[_buyer][_nftID] = _newStatus;
+        if (delivered[_buyer][_nftID] != _newStatus) {
+            emit productDelivered(_buyer, _nftID, _newStatus);
+        }
+    }
+
+    /** Update presential status
+      - only owner OR the verified carrier OR the selling contract can call this method
+      - at any buying operation, check if there is an entry at delivered
+        or presential with the SELLER'S ADDRESS AND THAT PARTICULAR NFT NUMBER 
+        and set both mappings to false
+        (it is important to prevent the approval of a subsequent sale to a
+        previous buyer before the delivery of the product)
+    */
+    function updatePresentialStatus(
+        address _buyer,
+        uint256 _nftID,
+        bool _newStatus
+    ) public {
+        presential[_buyer][_nftID] = _newStatus;
+        if (_newStatus) {
+            emit presentialSale(_buyer, _nftID, _newStatus);
+        }
+    }
 
     /** Return whether or not the sele was presential */
     function isPresential(address _buyer, uint256 _nftID)
@@ -49,18 +81,18 @@ contract ShippingInfo {
         returns (bool)
     {
         bool isPresentialSale = presential[_buyer][_nftID];
-        if (isPresentialSale) {
+        if (presential[_buyer][_nftID] != isPresentialSale) {
             emit presentialSale(_buyer, _nftID, isPresentialSale);
         }
         return isPresentialSale;
     }
 
     /** Return delivered state */
-    function isDelivered(address _buyer, uint256 _nftID) public returns (bool) {
-        bool wasDelivered = delivered[_buyer][_nftID];
-        if (wasDelivered) {
-            emit productDelivered(_buyer, _nftID, wasDelivered);
-        }
-        return wasDelivered;
+    function isDelivered(address _buyer, uint256 _nftID)
+        public
+        view
+        returns (bool)
+    {
+        return delivered[_buyer][_nftID];
     }
 }
