f39683e4d392f43dd0c1e8ad1b8aa05c24c9d263
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-02 11:40:02 -0300

    feat: add scroll to top

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { useLayoutEffect } from 'react'
 
 import HeroSection from '../components/ui/HeroSection'
 import LiveAuction from '../components/ui/Live-auction/LiveAuction'
 
 
 const Home = () => {
+
+  // @notice scroll to the top of the page
+  useLayoutEffect(() => {
+    window.scrollTo(0, 0)
+  });
+
   return (
     <>
       <HeroSection />
