fb16168b4ccd55a4aa16a17d4b77faf256d7931f
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-02 07:42:23 -0300

    bug: reset to template

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,20 +1,12 @@
 import React from 'react'
-import { Container } from 'reactstrap'
 
 import HeroSection from '../components/ui/HeroSection'
-import LiveAuction from '../components/ui/Live-auction/LiveAuction'
-import SellerSection from '../components/ui/Seller-section/SellerSection'
-//import TrendingSection from '../components/ui/Trending-section/Trending'
-import StepSection from '../components/ui/Step-section/StepSection'
+
 
 const Home = () => {
   return (
     <>
       <HeroSection />
-      <LiveAuction />
-      <SellerSection />
-      {/* <TrendingSection /> */}
-      <StepSection />
     </>
   )
 }
