e3f904d318db916915935985a8cc74e3460e31ef
Fabio P. Benjovengo <fabio.benjovengo@gmail.com>
2023-02-02 11:40:39 -0300

    style: section header styling

--- client/src/components/ui/Live-auction/LiveAuction.jsx
+++ client/src/components/ui/Live-auction/LiveAuction.jsx
@@ -10,32 +10,51 @@ import { NFT__DATA } from '../../../assets/data/data.js' // test data
 
 const LiveAuction = () => {
   return (
-    <section>
-      <Container>
-        <Row>
-          <Col lg='12' className='mb-4'>
-            <div className="live_auction__top d-flex align-items center justify-content-between">
-              <h3>Recently Added</h3>
-              <span className='explore__more'>
-                <button className="explore__btn d-flex align-items-center gap-2">
-                  <Link to='/market'>More Items</Link>
-                </button>
-              </span>
-            </div>
-          </Col>
-          
-            {
-              //<NftCard />
-              NFT__DATA.slice(0,4).map((_item)=>(
-                <Col lg='3' md='4' sm='6' key={_item.id}>
-                  <NftCard key={_item.id} item={_item} />
-                </Col>
-              ))
-            }
-          
-        </Row>
-      </Container>
-    </section>
+    <>
+{/*       <section>
+        <Container>
+          <Row>
+            <Col lg='12' className='mb-4'>
+              <div className="live_auction__top d-flex align-items-center justify-content-between">
+                <h3>Recently Added</h3>
+                <span className='explore__more'>
+                  <button className="explore__btn d-flex align-items-center gap-2">
+                    <Link to='/market'>More Items</Link>
+                  </button>
+                </span>
+              </div>
+            </Col>
+            
+              {
+                //<NftCard />
+                NFT__DATA.slice(0,4).map((_item)=>(
+                  <Col lg='3' md='4' sm='6' key={_item.id}>
+                    <NftCard key={_item.id} item={_item} />
+                  </Col>
+                ))
+              }
+            
+          </Row>
+        </Container>
+      </section> */}
+
+      <section className="selling__section">
+        <Container>
+          <Row className='selling__content justify-content-center'>
+            <Col xs="12" className="text-center">
+              <h3>What's new?</h3>
+            </Col>
+          </Row>
+          <Row className='selling__content justify-content-center'>
+            <Col xs="12" className="text-center">
+              <button className="explore__more__btn">More Items</button>
+            </Col>
+          </Row>
+
+        </Container>
+      </section>
+    </>
+
   )
 }
 
